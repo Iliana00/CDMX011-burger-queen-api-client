ad9cae5fc9bfdc7da106b93c480db5a653afd2ee
Iliana00 <carrasquedo.279@gmail.com>
2021-11-29 21:59:25 -0600

    van and breakfast vieew are done

---
+++ my-app/src/Components/menu/desayuno.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+import Agua from "../../img/agua.png"
+import "./desayuno.scss"
+ 
+
+export  class Desayuno extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        product:[]
+      }
+    };
+  
+    componentDidMount(){
+      let url = "https://my-json-server.typicode.com/iliana00/CDMX011-burger-queen-api-client/product?type=almuerzo"
+      fetch(url)
+      .then(resp => resp.json())
+      .then(data => {
+
+        let posts=data.map((product) => {
+            return ( 
+              <div className="menu-desayuno">
+                <div className="hr-padre">
+                <hr/> 
+             </div> 
+                <div className="hijo-desayuno">          
+                <img className="img-product" src={Agua} alt="logos food"/>
+                <h3>{product.name}</h3>
+                <button className="menos">-</button>
+                <p> {product.price}</p>   
+                <button className="mas">+</button>  
+                        
+               </div>
+                 
+              </div>                                      
+                                     
+            )
+                                                    
+        })
+        this.setState({posts:posts});
+      })
+    }
+    render() {
+      return (
+      <div className="desayuno">
+        <p>Precio</p>
+        {this.state.posts}
+      </div>
+      );
+    }
+  }
+  
--- my-app/src/Components/menu/menu.js
+++ my-app/src/Components/menu/menu.js
@@ -1,38 +1,19 @@
-import { Product } from "../product/product";
-import { authUser } from "../User";
-import BttnLogout from "../nav.js/BttnLogOut";
-import "../../Menu.scss"
-import Logo from "../../img/bq_logo.png";
-
+import BarraNav from "../nav/barranav";
+import { Desayuno } from "../menu/desayuno.js"
+import "./menu.scss"
 
 export default function Menu() {
-  const currentUser = authUser();
 
-  return (
-    <div className="cont-1">
-      <div className="cont-2" >
-        <img className="child-1" src={Logo} alt="Logo"></img>
-        <div className="center-self">
-          <button className="bttn-order">PEDIDOS</button>
+    return(
+       
+        <div className="container1">
+<BarraNav/>
+        
+        <div className="des-alm">
+        <Desayuno/>
         </div>
-        <div className="center-self">
-          <p id="child-3"> {currentUser?.email} </p>
+        <div className="comanda">
         </div>
-        <div className="center-self">
-        <BttnLogout />
         </div>
-          
-      </div>
-    <div><button className="bttn-breakfast">DESAYUNOS</button> 
-    <button className="bttn-lunch">ALMUERZOS</button>
-    </div>
-      <div >
-
-        <Product />
-
-      </div>
-
-    </div>
-
-  )
+    )
 }
